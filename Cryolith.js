6ca9cd668f408f619ad0ad0864b41d7ad38727c5
PuslarisQuantica <167034715+MaxxStellarShift@users.noreply.github.com>
2025-08-03 16:21:28 +0200

    Update Cryolith.js
    1.0.0 of Cryolith

--- Cryolith.js
+++ Cryolith.js
@@ -1,8 +1,9 @@
 // Mod Sandboxels - Humains Congelés
 // Permet de congeler/décongeler les humains sans les transformer en viande
 
+// Créer les nouveaux éléments congelés
 elements.frozen_head = {
-    color: "#4169E1", // Bleu royal pour simuler la couleur gelée
+    color: "#6495ED", // Bleu cornflower
     behavior: behaviors.POWDER,
     tempHigh: 0,
     stateHigh: "head",
@@ -11,11 +12,12 @@ elements.frozen_head = {
     density: 1500,
     hardness: 0.8,
     breakInto: "bone",
-    conduct: 0.1
+    conduct: 0.1,
+    desc: "Tête humaine congelée"
 };
 
 elements.frozen_body = {
-    color: "#4682B4", // Bleu acier pour le corps gelé
+    color: "#4682B4", // Bleu acier
     behavior: behaviors.POWDER,
     tempHigh: 0,
     stateHigh: "body", 
@@ -24,23 +26,37 @@ elements.frozen_body = {
     density: 1200,
     hardness: 0.8,
     breakInto: "bone",
-    conduct: 0.1
+    conduct: 0.1,
+    desc: "Corps humain congelé"
 };
 
-// Modifier l'élément "head" pour qu'il se transforme en "frozen_head" quand gelé
+// Supprimer complètement les réactions existantes qui causent frozen_meat
+delete elements.head.reactions;
+delete elements.body.reactions;
+
+// Recréer les réactions sans celles qui font frozen_meat
+elements.head.reactions = {};
+elements.body.reactions = {};
+
+// Ajouter les propriétés de température
 elements.head.tempLow = 0;
 elements.head.stateLow = "frozen_head";
 
-// Modifier l'élément "body" pour qu'il se transforme en "frozen_body" quand gelé
 elements.body.tempLow = 0;
 elements.body.stateLow = "frozen_body";
 
-// Empêcher la transformation en "frozen_meat" pour head et body
-if (elements.head.reactions) {
-    delete elements.head.reactions.ice;
-    delete elements.head.reactions.liquid_nitrogen;
-}
-if (elements.body.reactions) {
-    delete elements.body.reactions.ice;
-    delete elements.body.reactions.liquid_nitrogen;
-}
+// Forcer la suppression des réactions de glace existantes
+runAfterLoad(function() {
+    if (elements.head.reactions && elements.head.reactions.ice) {
+        delete elements.head.reactions.ice;
+    }
+    if (elements.head.reactions && elements.head.reactions.liquid_nitrogen) {
+        delete elements.head.reactions.liquid_nitrogen;
+    }
+    if (elements.body.reactions && elements.body.reactions.ice) {
+        delete elements.body.reactions.ice;
+    }
+    if (elements.body.reactions && elements.body.reactions.liquid_nitrogen) {
+        delete elements.body.reactions.liquid_nitrogen;
+    }
+});
